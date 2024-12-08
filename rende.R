f5166901918227ac22fcc80e244de9598661d073
Rafael Castro <ruevko@proton.me>
2024-12-08 17:58:48 -0500

    Create palestra dashboard

--- rende.R
+++ rende.R
@@ -1,1 +1,2 @@
-rmarkdown::render("index.Rmd", output_file = "docs/index.html")
+#rmarkdown::render("index.Rmd", output_file = "docs/index.html")
+rmarkdown::render("palestra2024.Rmd", output_file = "docs/palestra2024/index.html")
